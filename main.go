08c4fc69e97a4851be7b991c70dc56cd32bb72c2
Matt Sell <mwsell@gmail.com>
2017-06-04 10:07:39 -0500

    init commit

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var serverNameProvided bool
+	var serverName string
+
+	if len(os.Args) >= 2 {
+		serverNameProvided = true
+		serverName = os.Args[1]
+		fmt.Printf("Searching for deployments on %s...\n", serverName)
+	}
+
+	if serverNameProvided {
+		deployments := fetchDeployments(serverName)
+		prettyPrint(deployments)
+	}
+}
+
+/* Used to figure out the length of the longest string */
+func compareAgainstLongest(longest *int, current string) {
+	currentLength := len(current)
+	if currentLength > *longest {
+		*longest = currentLength
+	}
+}
+
+func padRight(str string, length int) string {
+	for {
+		str += " "
+		if len(str) > length {
+			return str[0:length]
+		}
+	}
+}
+
+func prettyPrint(deployments []deployment) {
+
+	var serverLen, applicationLen, branchLen, versionLen int
+
+	for _, d := range deployments {
+		compareAgainstLongest(&serverLen, d.server)
+		compareAgainstLongest(&applicationLen, d.application)
+		compareAgainstLongest(&branchLen, d.branch)
+		compareAgainstLongest(&versionLen, d.version)
+	}
+
+	// Print colunn headers
+	fmt.Printf("%s %s %s %s\n",
+		padRight("Server", serverLen),
+		padRight("App", applicationLen),
+		padRight("Branch", branchLen),
+		padRight("Version", versionLen))
+
+	for _, d := range deployments {
+		fmt.Printf("%s %s %s %s\n",
+			padRight(d.server, serverLen),
+			padRight(d.application, applicationLen),
+			padRight(d.branch, branchLen),
+			padRight(d.version, versionLen))
+	}
+}
+
+func fetchDeployments(serverName string) []deployment {
+	s1 := deployment{
+		server:      serverName,
+		application: "SPL",
+		branch:      "master",
+		version:     "3.0.0.2"}
+
+	s2 := deployment{
+		server:      serverName,
+		application: "Lead API",
+		branch:      "master",
+		version:     "1.3.0.9"}
+
+	return []deployment{s1, s2}
+}
+
+type deployment struct {
+	server      string
+	application string
+	branch      string
+	version     string
+}
