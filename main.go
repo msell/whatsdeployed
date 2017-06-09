656a3808b242027c6551f205347e4618bddafc89
Matt Sell <mwsell@gmail.com>
2017-06-08 23:22:00 -0500

    move printing out to utils

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"whatsdeployed/api"
 	"whatsdeployed/models"
+	"whatsdeployed/utils"
 )
 
 func main() {
@@ -20,51 +21,7 @@ func main() {
 
 	if serverNameProvided {
 		deployments := fetchDeployments(serverName)
-		prettyPrint(deployments)
-	}
-}
-
-/* Used to figure out the length of the longest string */
-func compareAgainstLongest(longest *int, current string) {
-	currentLength := len(current)
-	if currentLength > *longest {
-		*longest = currentLength
-	}
-}
-
-func padRight(str string, length int) string {
-	for {
-		str += " "
-		if len(str) > length {
-			return str[0:length]
-		}
-	}
-}
-
-func prettyPrint(deployments []models.Deployment) {
-
-	var serverLen, applicationLen, branchLen, versionLen int
-
-	for _, d := range deployments {
-		compareAgainstLongest(&serverLen, d.Server)
-		compareAgainstLongest(&applicationLen, d.Application)
-		compareAgainstLongest(&branchLen, d.Branch)
-		compareAgainstLongest(&versionLen, d.Version)
-	}
-
-	// Print colunn headers
-	fmt.Printf("%s %s %s %s\n",
-		padRight("Server", serverLen),
-		padRight("App", applicationLen),
-		padRight("Branch", branchLen),
-		padRight("Version", versionLen))
-
-	for _, d := range deployments {
-		fmt.Printf("%s %s %s %s\n",
-			padRight(d.Server, serverLen),
-			padRight(d.Application, applicationLen),
-			padRight(d.Branch, branchLen),
-			padRight(d.Version, versionLen))
+		utils.PrettyPrint(deployments)
 	}
 }
 
