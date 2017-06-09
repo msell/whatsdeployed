ab1949c838f1e36eaedf0130585e266dc6043e32
Matt Sell <mwsell@gmail.com>
2017-06-08 22:44:10 -0500

    move api calls into own package

---
+++ api/api.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"whatsdeployed/models"
+)
+
+// FetchApplications : Makes API call to fetch applications
+func FetchApplications(serverID int) []models.Application {
+	res, err := http.Get(fmt.Sprintf(
+		"%s%d%s",
+		"http://whatsdeployed.herokuapp.com/servers/",
+		serverID,
+		"/deployed_apps.json",
+	))
+
+	if err != nil {
+		log.Fatal("Could not fetch applications ", err)
+	}
+
+	defer res.Body.Close()
+
+	var apps []models.Application
+
+	err = json.NewDecoder(res.Body).Decode(&apps)
+	if err != nil {
+		log.Fatal("Could not decode applications json ", err)
+	}
+
+	fmt.Println(apps)
+	return apps
+}
+
+// FetchServerID : Make api call to get servers and find serverID based on server name
+func FetchServerID(serverName string) int {
+	res, err := http.Get("http://whatsdeployed.herokuapp.com/servers.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	var servers []models.Server
+
+	err = json.NewDecoder(res.Body).Decode(&servers)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serverID int
+	for _, s := range servers {
+		if s.Name == strings.ToUpper(serverName) {
+			serverID = s.ID
+		}
+	}
+
+	if serverID == 0 {
+		log.Fatal("Server " + serverName + " does not exist")
+	}
+
+	return serverID
+}
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
-	"strings"
+	"whatsdeployed/api"
 	"whatsdeployed/models"
 )
 
@@ -71,62 +68,11 @@ func prettyPrint(deployments []models.Deployment) {
 	}
 }
 
-func fetchApplications(serverID int) []models.Application {
-	res, err := http.Get(fmt.Sprintf(
-		"%s%d%s",
-		"http://whatsdeployed.herokuapp.com/servers/",
-		serverID,
-		"/deployed_apps.json",
-	))
-
-	if err != nil {
-		log.Fatal("Could not fetch applications ", err)
-	}
-
-	defer res.Body.Close()
-
-	var apps []models.Application
-
-	err = json.NewDecoder(res.Body).Decode(&apps)
-	if err != nil {
-		log.Fatal("Could not decode applications json ", err)
-	}
-
-	fmt.Println(apps)
-	return apps
-}
-
 func fetchDeployments(serverName string) []models.Deployment {
-	res, err := http.Get("http://whatsdeployed.herokuapp.com/servers.json")
-	if err != nil {
-		log.Fatal(err)
-	}
+	serverID := api.FetchServerID(serverName)
+	api.FetchApplications(serverID)
 
-	defer res.Body.Close()
-
-	var servers []models.Server
-
-	err = json.NewDecoder(res.Body).Decode(&servers)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var serverID int
-	for _, s := range servers {
-		if s.Name == strings.ToUpper(serverName) {
-			serverID = s.ID
-		}
-	}
-
-	if serverID == 0 {
-		log.Fatal("Server " + serverName + " does not exist")
-	}
-
-	fmt.Println(serverID)
-
-	fetchApplications(serverID)
-
+	// TODO: iterate over results and map array of deployments
 	s1 := models.Deployment{
 		Server:      serverName,
 		Application: "SPL",
